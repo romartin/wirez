8b6c43224f6ccaec6316fc6fbb85e42a8626b8d9
Roger Martinez <roger600@gmail.com>
2016-02-11 22:35:36 +0100

    toolbox commands

--- wirez-core/wirez-core-client/src/main/java/org/wirez/core/client/util/ShapeUtils.java
+++ wirez-core/wirez-core-client/src/main/java/org/wirez/core/client/util/ShapeUtils.java
@@ -82,10 +82,13 @@ public class ShapeUtils {
         
         return new int[] { sMagnet, tMagnet };
     }
-    
+
+    /**
+     * Returns the distance between two points in a dual axis cartesian graph. 
+     */
     public static double dist(final double x0, final double y0, final double x1, final double y1) {
         final double dx = Math.abs( x1 - x0 );
         final double dy = Math.abs( y1 - y0 );
-        return ( ( dx * dx ) + ( dy * dy ) );
+        return ( Math.sqrt( ( dx * dx ) + ( dy * dy ) ) );
     }
 }

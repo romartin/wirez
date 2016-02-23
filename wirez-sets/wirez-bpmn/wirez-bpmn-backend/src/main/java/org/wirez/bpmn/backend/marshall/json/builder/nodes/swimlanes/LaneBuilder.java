44d17991450d1f06a72a28e9ff751b653dfdd716
Roger Martinez <roger600@gmail.com>
2016-02-23 23:02:04 +0100

    Backend - BPMN marshall / unmarshall

---
+++ wirez-sets/wirez-bpmn/wirez-bpmn-backend/src/main/java/org/wirez/bpmn/backend/marshall/json/builder/nodes/swimlanes/LaneBuilder.java
@@ -0,0 +1,36 @@
+package org.wirez.bpmn.backend.marshall.json.builder.nodes.swimlanes;
+
+import org.wirez.bpmn.api.Lane;
+import org.wirez.bpmn.api.Task;
+import org.wirez.bpmn.backend.marshall.json.builder.AbstractNodeBuilder;
+import org.wirez.core.api.graph.Edge;
+import org.wirez.core.api.graph.Node;
+import org.wirez.core.api.graph.content.view.View;
+
+import javax.enterprise.context.Dependent;
+
+@Dependent
+public class LaneBuilder extends AbstractNodeBuilder<Lane, Node<View<Lane>, Edge>>  {
+
+    public LaneBuilder() {
+        super();
+    }
+
+    @Override
+    public String getDefinitionId() {
+        return Lane.ID;
+    }
+
+    @Override
+    protected void setSize(BuilderContext context, Node<View<Lane>, Edge> node, double width, double height) {
+        super.setSize(context, node, width, height);
+        Lane def = node.getContent().getDefinition();
+        def.getWidth().setValue(width);
+        def.getHeight().setValue(height);
+    }
+
+    @Override
+    public String toString() {
+        return "[NodeBuilder=LaneBuilder]" + super.toString();
+    }
+}

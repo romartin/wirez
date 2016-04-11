8ba162aae85d86b7233153fdf6af208f4a441b43
Roger Martinez <roger600@gmail.com>
2016-04-11 17:32:05 +0200

    Form modeler integration

--- wirez-sets/wirez-bpmn/wirez-bpmn-api/src/main/java/org/wirez/bpmn/api/StartNoneEvent.java
+++ wirez-sets/wirez-bpmn/wirez-bpmn-api/src/main/java/org/wirez/bpmn/api/StartNoneEvent.java
@@ -19,6 +19,7 @@ package org.wirez.bpmn.api;
 import org.jboss.errai.common.client.api.annotations.MapsTo;
 import org.jboss.errai.common.client.api.annotations.Portable;
 import org.jboss.errai.databinding.client.api.Bindable;
+import org.livespark.formmodeler.metaModel.FieldDef;
 import org.wirez.bpmn.api.property.Radius;
 import org.wirez.bpmn.api.property.general.BPMNGeneral;
 import org.wirez.bpmn.api.property.general.BackgroundSet;
@@ -62,6 +63,7 @@ public class StartNoneEvent implements BPMNDefinition {
     private CatchEventAttributes catchEventAttributes;
     
     @Property
+    @FieldDef(label = "Radius", property = "value")
     private Radius radius;
 
     @Labels

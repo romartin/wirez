e886a69e657d50de31816c19432dfe616c0715e7
roger <roger600@gmail.com>
2016-09-29 20:03:19 +0200

    fixing.

--- wirez-core/wirez-commons/wirez-client-common/src/main/java/org/wirez/core/client/canvas/util/CanvasLayoutUtils.java
+++ wirez-core/wirez-commons/wirez-client-common/src/main/java/org/wirez/core/client/canvas/util/CanvasLayoutUtils.java
@@ -22,7 +22,7 @@ import java.util.Iterator;
 @Dependent
 public class CanvasLayoutUtils {
 
-    private static final int DISTANCE = 100;
+    private static final int PADDING = 50;
     private static final float MARGIN = 0.2f;
 
     public static boolean isCanvasRoot( final Diagram diagram,
@@ -39,8 +39,7 @@ public class CanvasLayoutUtils {
     public double[] getNextLayoutPosition( final CanvasHandler canvasHandler, final Element<View<?>> source ) {
 
         final Double[] pos = GraphUtils.getPosition( source.getContent() );
-        final Double[] size = GraphUtils.getSize( source.getContent() );
-        final double[] next = new double[] { pos[0] + size[0] + DISTANCE, pos[1] };
+        final double[] next = new double[] { pos[0], pos[1] };
 
         return checkNextLayoutPosition( next[0], next[1], canvasHandler );
     }
@@ -65,8 +64,8 @@ public class CanvasLayoutUtils {
 
         if ( x >= getBound( lr.getX() ) ) {
 
-            result[0] = DISTANCE;
-            result[1] += DISTANCE;
+            result[0] = PADDING;
+            result[1] += PADDING;
 
         }
 
@@ -120,10 +119,10 @@ public class CanvasLayoutUtils {
             private void onStartNodeTraversal( final Iterator<Node<View, Edge>> parents,
                                                final Node<View, Edge> node ) {
 
-                double parentX = 0;
-                double parentY = 0;
-
                 if ( null != parents && parents.hasNext() ) {
+
+                    double parentX = 0;
+                    double parentY = 0;
 
                     while ( parents.hasNext() ) {
 
@@ -153,9 +152,24 @@ public class CanvasLayoutUtils {
 
                     }
 
+                } else if ( null != node ) {
+
+                    final Double[] nodeCoords = GraphUtils.getPosition( node.getContent() );
+                    final Double[] nodeSize = GraphUtils.getSize( node.getContent() );
+
+                    if ( nodeCoords[0] > getCurrentMaxX() ) {
+
+                        result[0] = nodeCoords[0] + nodeSize[0] + PADDING;
+
+                    }
+
+                    if ( nodeCoords[1] > getCurrentMaxY() ) {
+
+                        result[1] = nodeCoords[1] + nodeSize[1] + PADDING;
+
+                    }
+
                 }
-
-
 
             }
 

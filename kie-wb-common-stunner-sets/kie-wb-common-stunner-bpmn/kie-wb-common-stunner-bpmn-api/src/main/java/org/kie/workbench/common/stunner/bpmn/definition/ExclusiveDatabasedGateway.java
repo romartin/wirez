29d149dd526cf560f426d154dbe620340a91d892
Jeremy Lindop <jlindop@redhat.com>
2016-10-03 10:58:24 +0100

    Define properties for XOR Gateway

--- kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-api/src/main/java/org/kie/workbench/common/stunner/bpmn/definition/ExclusiveDatabasedGateway.java
+++ kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-api/src/main/java/org/kie/workbench/common/stunner/bpmn/definition/ExclusiveDatabasedGateway.java
@@ -16,17 +16,22 @@
 
 package org.kie.workbench.common.stunner.bpmn.definition;
 
+import javax.validation.Valid;
+
 import org.jboss.errai.common.client.api.annotations.MapsTo;
 import org.jboss.errai.common.client.api.annotations.NonPortable;
 import org.jboss.errai.common.client.api.annotations.Portable;
 import org.jboss.errai.databinding.client.api.Bindable;
+import org.kie.workbench.common.forms.metaModel.FieldDef;
 import org.kie.workbench.common.stunner.bpmn.definition.property.background.BackgroundSet;
 import org.kie.workbench.common.stunner.bpmn.definition.property.dimensions.CircleDimensionSet;
 import org.kie.workbench.common.stunner.bpmn.definition.property.font.FontSet;
+import org.kie.workbench.common.stunner.bpmn.definition.property.gateway.ExclusiveGatewayExecutionSet;
 import org.kie.workbench.common.stunner.bpmn.definition.property.general.BPMNGeneral;
 import org.kie.workbench.common.stunner.bpmn.shape.def.ExclusiveDatabasedGatewayShapeDef;
 import org.kie.workbench.common.stunner.core.definition.annotation.Definition;
 import org.kie.workbench.common.stunner.core.definition.annotation.Description;
+import org.kie.workbench.common.stunner.core.definition.annotation.PropertySet;
 import org.kie.workbench.common.stunner.core.definition.annotation.Shape;
 import org.kie.workbench.common.stunner.core.definition.annotation.definition.Title;
 import org.kie.workbench.common.stunner.core.definition.annotation.morph.Morph;
@@ -46,12 +51,21 @@ public class ExclusiveDatabasedGateway extends BaseGateway {
     @Description
     public static final transient String description = "Exclusive Data-based Gateway";
 
+    @PropertySet
+    @FieldDef( label = "Implementation/Execution", position = 1)
+    @Valid
+    ExclusiveGatewayExecutionSet executionSet;
+
+    private static long nextID = 0;
+    private long Id = 0;
+
     @NonPortable
     public static class ExclusiveDatabasedGatewayBuilder extends BaseGatewayBuilder<ExclusiveDatabasedGateway> {
 
         @Override
         public ExclusiveDatabasedGateway build() {
             return new ExclusiveDatabasedGateway(  new BPMNGeneral( "gw" ),
+                    new ExclusiveGatewayExecutionSet(),
                     new BackgroundSet( COLOR, BORDER_COLOR, BORDER_SIZE ),
                     new FontSet(),
                     new CircleDimensionSet( RADIUS ) );
@@ -60,16 +74,18 @@ public class ExclusiveDatabasedGateway extends BaseGateway {
     }
 
     public ExclusiveDatabasedGateway() {
-
+        this.Id = nextID++;
     }
 
     public ExclusiveDatabasedGateway(@MapsTo("general") BPMNGeneral general,
+                                     @MapsTo("executionSet") ExclusiveGatewayExecutionSet executionSet,
                                      @MapsTo("backgroundSet") BackgroundSet backgroundSet,
                                      @MapsTo("fontSet") FontSet fontSet,
                                      @MapsTo("dimensionsSet") CircleDimensionSet dimensionsSet) {
 
         super( general, backgroundSet, fontSet, dimensionsSet );
-
+        this.executionSet = executionSet;
+        this.Id = nextID++;
     }
 
     public String getTitle() {
@@ -78,6 +94,22 @@ public class ExclusiveDatabasedGateway extends BaseGateway {
 
     public String getDescription() {
         return description;
+    }
+
+    public ExclusiveGatewayExecutionSet getExecutionSet() {
+        return executionSet;
+    }
+
+    public void setExecutionSet(ExclusiveGatewayExecutionSet executionSet) {
+        this.executionSet = executionSet;
+    }
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long Id) {
+        this.Id = Id;
     }
 
 }

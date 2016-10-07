471bf8ad2d8ae02051aeb88aa9a996767b5ede46
Jeremy Lindop <jlindop@redhat.com>
2016-10-07 13:18:43 +0100

    Fix marshalling of Simulation Properties

--- kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/main/java/org/kie/workbench/common/stunner/bpmn/backend/marshall/json/oryx/Bpmn2OryxIdMappings.java
+++ kie-wb-common-stunner-sets/kie-wb-common-stunner-bpmn/kie-wb-common-stunner-bpmn-backend/src/main/java/org/kie/workbench/common/stunner/bpmn/backend/marshall/json/oryx/Bpmn2OryxIdMappings.java
@@ -12,6 +12,11 @@ import org.kie.workbench.common.stunner.bpmn.definition.property.event.TimeCycle
 import org.kie.workbench.common.stunner.bpmn.definition.property.event.TimeDate;
 import org.kie.workbench.common.stunner.bpmn.definition.property.event.TimeDuration;
 import org.kie.workbench.common.stunner.bpmn.definition.property.general.Name;
+import org.kie.workbench.common.stunner.bpmn.definition.property.simulation.DistributionType;
+import org.kie.workbench.common.stunner.bpmn.definition.property.simulation.StandardDeviation;
+import org.kie.workbench.common.stunner.bpmn.definition.property.simulation.TimeUnit;
+import org.kie.workbench.common.stunner.bpmn.definition.property.simulation.UnitCost;
+import org.kie.workbench.common.stunner.bpmn.definition.property.simulation.WorkingHours;
 import org.kie.workbench.common.stunner.bpmn.definition.property.task.CalledElement;
 import org.kie.workbench.common.stunner.bpmn.definition.property.task.RuleFlowGroup;
 import org.kie.workbench.common.stunner.bpmn.definition.property.task.ScriptLanguage;
@@ -55,6 +60,13 @@ public class Bpmn2OryxIdMappings {
         put( TimeDuration.class, "timeduration" );
         put( TimeCycle.class, "timecycle" );
         put( TimeCycleLanguage.class, "timecyclelanguage" );
+
+        // Simulation properties
+        put(TimeUnit.class, "timeunit");
+        put(StandardDeviation.class, "standarddeviation");
+        put(DistributionType.class, "distributiontype");
+        put(WorkingHours.class, "workinghours");
+        put(UnitCost.class, "unitcost");
     }};
 
 
